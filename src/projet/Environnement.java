f6eb77827606962fa54f87625f146c4fa67b77ec
Lucie <PC@10.11.8.63>
2019-10-04 09:58:12 +0200

    Modif environnement et robots

--- src/projet/Environnement.java
+++ src/projet/Environnement.java
@@ -6,73 +6,19 @@ import lejos.robotics.Color;
 
 public class Environnement { 
 	
-	private ArrayList<CaseEnvironnement> env = new ArrayList<CaseEnvironnement>();
+	//private ArrayList<CaseEnvironnement> env = new ArrayList<CaseEnvironnement>();
 	private CaseEnvironnement start;
 	private CaseEnvironnement caseActuel;
-	private CaseEnvironnement carte [][];
-	private boolean t;
+	private CaseEnvironnement [][] carte ;
+
 	
-	public Environnement (ArrayList<CaseEnvironnement> map, CaseEnvironnement s){
+	public Environnement ( CaseEnvironnement s){
 		start = s;
 		caseActuel = start;
-		env = map;
+		//carte = c;
 		// this.env = new CaseEnvironnement[7][5]; 
-		
-	
-			carte = new CaseEnvironnement [6][4];	
-			if (t == true) //Winterfell
-			{
-				carte [0][0]= Color.RED; //Camp militaire nord ouest 
-				carte [0][1]= Color.BLUE;
-				carte [1][0]= Color.GREEN;
-				carte [1][1]= Color.BLUE;
-				carte [2][0]= Color.GREEN;
-				carte [2][1]= Color.BLUE;
-				carte [2][2]= Color.BLUE;
-				carte [3][0]= Color.GREEN;
-				carte [3][1]= Color.GREEN;
-				carte [3][2]= Color.BLUE;
-				carte [4][0]= Color.GREEN;
-				carte [4][1]= Color.ORANGE;
-				carte [4][2]= Color.ORANGE;
-				carte [4][3]= Color.ORANGE;
-				carte [5][0]= Color.GREEN;
-				carte [5][1]= Color.GREEN;
-				carte [5][2]= Color.GREEN;
-				carte [5][3]= Color.RED; //camp militaire sud
-				carte [5][4]= Color.BLUE;
-				carte [6][0]= Color.WHITE; //Départ Winterfell
-				carte [6][1]= Color.GREEN;
-				carte [6][2]= Color.GREEN;
-				carte [6][3]= Color.GREEN;
-				carte [6][5]= Color.BLUE;
-			}
-			else if (t == false) //Thenn
-			{ 
-				carte [0][0]= Color.RED; // Camp militaire nord ouest 
-				carte [0][1]= Color.BLUE;
-				carte [0][2]= Color.GREEN;
-				carte [0][3]= Color.GREEN;
-				carte [0][4]= Color.WHITE; // Départ Thenn
-				carte [1][1]= Color.BLUE;
-				carte [1][2]= Color.GREEN;
-				carte [1][3]= Color.GREEN;
-				carte [1][4]= Color.GREEN;
-				carte [2][1]= Color.BLUE;
-				carte [2][2]= Color.BLUE;
-				carte [2][3]= Color.GREEN;
-				carte [2][4]= Color.ORANGE;
-				carte [3][2]= Color.BLUE;
-				carte [3][3]= Color.GREEN;
-				carte [3][4]= Color.GREEN;
-				carte [4][3]= Color.ORANGE;
-				carte [4][4]= Color.GREEN;
-				carte [5][3]= Color.RED; // Camp militaire sud
-				carte [5][4]= Color.BLUE;
-				carte [6][4]= Color.BLUE;
-			}
-		
 	}
+	
 	public CaseEnvironnement getCase(){
 		return caseActuel;
 	}
@@ -80,4 +26,10 @@ public class Environnement {
 		caseActuel.setColor(c);
 		caseActuel.setCost(cos);
 	}
+	
+	public CaseEnvironnement [][] getEnvironnement(){
+		return carte;
+	}
+	
+	
 }
--- src/projet/Robot.java
+++ src/projet/Robot.java
@@ -1,5 +1,7 @@
 package projet;
 
+import lejos.robotics.Color;
+
 public class Robot {
 
 	private String nom;	
@@ -19,4 +21,61 @@ public class Robot {
 	public void setTeam(boolean newTeam){
 		this.team= newTeam;
 	}
+	
+	public void CreationMap() {
+		CaseEnvironnement [][] carte ;
+		carte = env.getEnvironnement();
+		if (team == true) //Winterfell
+		{
+			carte [0][0]= new CaseEnvironnement(Color.RED); //Camp militaire nord ouest 
+			carte [0][1]= new CaseEnvironnement(Color.BLUE);
+			carte [1][0]= new CaseEnvironnement(Color.GREEN);
+			carte [1][1]= new CaseEnvironnement(Color.BLUE);
+			carte [2][0]= new CaseEnvironnement(Color.GREEN);
+			carte [2][1]= new CaseEnvironnement(Color.BLUE);
+			carte [2][2]= new CaseEnvironnement(Color.BLUE);
+			carte [3][0]= new CaseEnvironnement(Color.GREEN);
+			carte [3][1]= new CaseEnvironnement(Color.GREEN);
+			carte [3][2]= new CaseEnvironnement(Color.BLUE);
+			carte [4][0]= new CaseEnvironnement(Color.GREEN);
+			carte [4][1]= new CaseEnvironnement(Color.ORANGE);
+			carte [4][2]= new CaseEnvironnement(Color.ORANGE);
+			carte [4][3]= new CaseEnvironnement(Color.ORANGE);
+			carte [5][0]= new CaseEnvironnement(Color.GREEN);
+			carte [5][1]= new CaseEnvironnement(Color.GREEN);
+			carte [5][2]= new CaseEnvironnement(Color.GREEN);
+			carte [5][3]= new CaseEnvironnement(Color.RED); //camp militaire sud
+			carte [5][4]= new CaseEnvironnement(Color.BLUE);
+			carte [6][0]= new CaseEnvironnement(Color.WHITE); //Départ Winterfell
+			carte [6][1]= new CaseEnvironnement(Color.GREEN);
+			carte [6][2]= new CaseEnvironnement(Color.GREEN);
+			carte [6][3]= new CaseEnvironnement(Color.GREEN);
+			carte [6][5]= new CaseEnvironnement(Color.BLUE);
+		}
+		else //Thenn
+		{ 
+			carte [0][0]= new CaseEnvironnement(Color.RED); // Camp militaire nord ouest 
+			carte [0][1]= new CaseEnvironnement(Color.BLUE);
+			carte [0][2]= new CaseEnvironnement(Color.GREEN);
+			carte [0][3]= new CaseEnvironnement(Color.GREEN);
+			carte [0][4]= new CaseEnvironnement(Color.WHITE); // Départ Thenn
+			carte [1][1]= new CaseEnvironnement(Color.BLUE);
+			carte [1][2]= new CaseEnvironnement(Color.GREEN);
+			carte [1][3]= new CaseEnvironnement(Color.GREEN);
+			carte [1][4]= new CaseEnvironnement(Color.GREEN);
+			carte [2][1]= new CaseEnvironnement(Color.BLUE);
+			carte [2][2]= new CaseEnvironnement(Color.BLUE);
+			carte [2][3]= new CaseEnvironnement(Color.GREEN);
+			carte [2][4]= new CaseEnvironnement(Color.ORANGE);
+			carte [3][2]= new CaseEnvironnement(Color.BLUE);
+			carte [3][3]= new CaseEnvironnement(Color.GREEN);
+			carte [3][4]= new CaseEnvironnement(Color.GREEN);
+			carte [4][3]= new CaseEnvironnement(Color.ORANGE);
+			carte [4][4]= new CaseEnvironnement(Color.GREEN);
+			carte [5][3]= new CaseEnvironnement(Color.RED); // Camp militaire sud
+			carte [5][4]= new CaseEnvironnement(Color.BLUE);
+			carte [6][4]= new CaseEnvironnement(Color.BLUE);
+		}
+}
+
 }
